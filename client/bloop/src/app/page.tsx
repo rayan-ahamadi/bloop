d8165a485410a86ff73b431b95a19aa75aa3a5e2
rayan-ahamadi <rayan.ahamadi@laplateforme.io>
2025-06-30 14:16:04 +0200

    feat(front): landing page integration

--- client/bloop/src/app/page.tsx
+++ client/bloop/src/app/page.tsx
@@ -1,103 +1,76 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+//import { Dialog } from "@/components/ui/dialog";
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="flex flex-col h-full">
+      <div id="title-box" className="bg-secondary-dark/100 text-secondary/100 p-8 flex-grow-1 flex flex-col justify-between">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src="/images/logo/yeuxbloop.png"
+          alt="Bloop Logo"
+          width={50}
+          height={50}
+          className="mx-auto mb-4"
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <h1 className="font-[900] text-[88px] text-left" style={{ lineHeight: "60%" }}>
+        BIENVENUE SUR
+          <Image
+            src="/images/logo/bloop.png"
+            alt="Bloop Logo"
+            width={200}
+            height={50}
+            className="inline-block ml-3 align-middle translate-y-[-9%]"
+          />
+        </h1>
+      </div>
+      <div id="button-box" className="flex flex-col gap-3 bg-secondary/100 text-secondary-dark/100 flex-grow-1 ">
+        <div id="apis" className="p-8 flex flex-col gap-3">
+          <Button className="text-[17px] flex flex-row justify-center items-center items-center">
+            {/* <Image 
+              src="/images/icons/Google.png"
+              alt="google-logo"
+              height={30}
+              width={30}
+            /> */}
+            Continuer avec Google
+          </Button>
+          <Button className="text-[17px] flex flex-row justify-center items-center">
+            {/* <Image 
+              src="/images/icons/Apple Inc.png"
+              alt="apple-logo"
+              height={30}
+              width={30}
+            /> */}
+            Continuer avec Apple
+          </Button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        
+       
+        <div id="separator" className="flex w-full">
+          <hr className="flex-grow border-t border-secondary-dark/100" />
+        </div>
+
+        <div id="email" className="p-8 flex flex-col gap-3 h-full ">
+          <Button className="mb-5 text-[17px] flex flex-row justify-center items-center">
+            {/* <Image 
+              src="/images/icons/+.png"
+              alt="+ icons"
+              height={30}
+              width={30}
+            /> */}
+            Créez un compte
+          </Button>
+
+          <p className="text-[14px]">En vous inscrivant, vous acceptez nos <Link href={"#"} className="text-primary">Conditions d’utilisation</Link> , notre <Link href={"#"} className="text-primary">Politique de confidencialité</Link>  et notre <Link href={"#"} className="text-primary">Utilisation des cookies</Link>.</p>
+          <p className="text-[14px] mt-15">Vous avez déjà un compte ? <Link href={"#"} className="text-primary">Connectez-vous</Link> </p>
+        </div>
+
+        
+      </div>
+      
     </div>
   );
 }
